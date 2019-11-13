d963dbfb93e0d24d315d13273503348c8f868c5e
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-13 23:24:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,27 +1,21 @@
 def sort_array_asc(asc_arg)
   return asc_arg.sort
 end
-
 def sort_array_desc(desc_arg)
   return desc_arg.sort.reverse
 end
-
 def sort_array_char_count(char_arg)
   return char_arg.sort {|word1, word2| word1.size <= word2.size ? -1 : 1 }
 end
-
 def swap_elements(swap_arg)
   temp = swap_arg[1]
-<<<<<<< HEAD
   swap_arg[1] = swap_arg[2]
   swap_arg[2] = temp
   return swap_arg
 end
-
 def reverse_array(arg_rever)
  return arg_rever.reverse
 end
-
 def kesha_maker(dollarsign)
   kesha_new = []
   dollarsign.each do |dollaword| dollaword[2] = '$'
@@ -29,42 +23,12 @@ def kesha_maker(dollarsign)
   end
   return kesha_new
 end
-
  def find_a(words)
    return words.filter {|word| word[0] == "a"}
  end
-
  def sum_array(additon)
     return additon.sum
  end
-
 def add_s(s_words)
  return s_words.each_with_index.collect {|element, index| index == 1 ? element : "#{element}s"}
 end
-=======
-  cho = "awesome"
-  swap_arg[1] = swap_arg[2]
-  swap_arg[1] = temp
-  return swap_arg
-end
-#
-# def reverse_array
-#
-# end
-#
-# def kesha_maker
-#
-# end
-#
-# def find_a
-#
-# end
-#
-# def sum_array
-#
-# end
-#
-# def add_s
-#
-# end
->>>>>>> 1b454ba1c07476349b3326439d3bca5b63bd9bcc
